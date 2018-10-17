43ad5e114432a09551545bebbb6480b5232a6a2b
denevers <denevers1649@gmail.com>
2018-10-17 13:10:17 -0400

    removed CORS control from the code to let the configuration on server

--- src/nrcan/lms/gsc/gsip/CORSFilter.java
+++ src/nrcan/lms/gsc/gsip/CORSFilter.java
@@ -13,12 +13,15 @@ public class CORSFilter implements ContainerResponseFilter {
 	 @Override
 	    public void filter(ContainerRequestContext request,
 	            ContainerResponseContext response) throws IOException {
+		 
+		 /** ticket #3310
 	        response.getHeaders().add("Access-Control-Allow-Origin", "*");
 	        response.getHeaders().add("Access-Control-Allow-Headers",
 	                "origin, content-type, accept, authorization");
 	        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
 	        response.getHeaders().add("Access-Control-Allow-Methods",
 	                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	                **/
 	    }
 
 }
