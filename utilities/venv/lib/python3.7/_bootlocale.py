8a782c89bef8e0624cc823af13bd217e0662b759
denevers <denevers1649@gmail.com>
2019-12-04 21:13:54 -0500

    commited python stuff from Joos. not sure it should be in there

--- utilities/venv/lib/python3.7/_bootlocale.py
+++ utilities/venv/lib/python3.7/_bootlocale.py
@@ -1,1 +1,1 @@
-/usr/local/Cellar/python/3.7.3/Frameworks/Python.framework/Versions/3.7/lib/python3.7/_bootlocale.py
+C:/usr/local/Cellar/python/3.7.3/Frameworks/Python.framework/Versions/3.7/lib/python3.7/_bootlocale.py
