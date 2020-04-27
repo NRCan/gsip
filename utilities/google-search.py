e3813964d4d5f0cbd009bd3a438a5db950a898ba
denevers <denevers1649@gmail.com>
2020-04-27 06:57:48 -0400

    removed old python harvester

--- utilities/google-search.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from googlesearch import search
-
-query = "https://geoconnex.ca/gsip/info/LOD_Node/CAN_Hydro_LOD_Node"
-
-for j in search(query, tld="co.in", num=10, stop=1, pause=2):
-  print(j)
